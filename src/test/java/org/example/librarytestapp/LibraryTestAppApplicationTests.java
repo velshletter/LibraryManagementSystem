52fa642bea263058c45f7b7e1413215b2059569b
velshletter <velshletter@gmail.com>
2025-01-22 00:06:02 +0300

    Initial Commit

---
+++ src/test/java/org/example/librarytestapp/LibraryTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.librarytestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
